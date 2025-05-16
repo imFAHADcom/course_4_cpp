9a7463a1e0289590bcb10b3a23939196f3aa5bd4
imFAHADcom <contact@imfahad.com>
2025-05-16 17:10:34 +0300

    ADD letter_pattern.cpp

---
+++ letter_pattern.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+
+int readPositiveNumber(std::string message){
+	int number = 0;
+	do{
+		std::cout << message << std::endl;
+		std::cin >> number;
+	} while(number < 0);
+
+	return number;
+}
+
+
+void printLetterPattern(int number){
+	std::cout << "***********\n";
+
+	for(int i = 65; i <= (65 + number - 1); i++){
+		for(int j = 1; j <= (i - 65 + 1) ; j++){
+			std::cout << char(i);
+		}
+		std::cout << std::endl;
+	}
+}
+
+
+int main(){
+
+	printLetterPattern(readPositiveNumber("Enter a number, Please?"));
+
+	return 0;
+}
