23373250e8ceba75861057c9672eddf592ec95fb
imFAHADcom <contact@imfahad.com>
2025-05-20 19:11:58 +0300

    ADD random_3_numbers_from_1_to_10.cpp

---
+++ random_3_numbers_from_1_to_10.cpp
@@ -0,0 +1,36 @@
+/*
+
+Write a program to print 3 random numbers from 1 to 10.
+
+output:
+9
+5
+3
+
+
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+
+int randomNumber(int from, int to){
+
+	int randNum = rand() % (to - from + 1) + from;
+
+	return randNum;
+}
+
+
+int main(){
+
+	srand((unsigned)time(NULL));
+
+	std::cout << randomNumber(1, 5) << std::endl;
+	std::cout << randomNumber(1, 5) << std::endl;
+	std::cout << randomNumber(1, 5) << std::endl;
+
+	return 0;
+}
+
+
