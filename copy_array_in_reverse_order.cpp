5af70acb938a13c412b601990fe2466da8d3c447
imFAHADcom <contact@imfahad.com>
2025-08-17 19:32:24 +0300

    UPD 2 copy_array_in_reverse_order.cpp

--- copy_array_in_reverse_order.cpp
+++ copy_array_in_reverse_order.cpp
@@ -1,3 +1,22 @@
+/*
+
+Write a program to fill array with max size 100 with random numbers from
+1 to 100, copy it to another array in reverse order and print it.
+
+Input
+10
+
+Output:
+
+Array 1 elements:
+64 8 62 19 2 21 15 74 96 85
+
+Array 2 elements after copying array 1 in reversed order:
+85 96 74 15 21 2 19 62 8 64
+
+*/
+
+
 #include <iostream>
 #include <string>
 #include <cstdlib>
